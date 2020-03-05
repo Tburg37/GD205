34cc6b7b4fdd74ded1ae5b0f1bb431a75a8e0116
BrianBrito239 <61032024+BrianBrito239@users.noreply.github.com>
2020-03-05 17:12:05 -0500

    HW

---
+++ Assets/Code/thirdpersoncamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirdpersoncamera : MonoBehaviour
+   
+{
+    float yaw;
+    float pitch;
+    public float mouseSensitivity = 10;
+    public Transform target;
+    public float dstFromTarget = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        Vector3 targetRotation = new Vector3(pitch, yaw);
+        transform.eulerAngles = targetRotation;
+        transform.position = target.position - transform.forward * dstFromTarget;
+        
+    }
+}
